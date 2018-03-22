73ed7b72ea00d6543ec984594896c59e68c883d9
Eric <xiaoxiang.cn@gmail.com>
2018-03-22 15:02:13 +0800

    0.2.2 => 0.3.0)

--- oriole_test/__init__.py
+++ oriole_test/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Eric.Zhou"""
 __email__ = 'xiaoxiang.cn@gmail.com'
-__version__ = '0.2.2'
+__version__ = '0.3.0'
--- setup.py
+++ setup.py
@@ -35,6 +35,6 @@ setup(
     packages=find_packages(include=['oriole_test']),
     test_suite='tests',
     url='https://github.com/zhouxiaoxiang/oriole-test',
-    version='0.2.2',
+    version='0.3.0',
     zip_safe=False,
 )
