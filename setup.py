a5571b227f420fe0e0c7a5cedb353cf2e189742e
Eric <xiaoxiang.cn@gmail.com>
2018-03-22 06:43:51 -0700

    0.4.0 => 0.4.1

--- setup.py
+++ setup.py
@@ -36,6 +36,6 @@ setup(
     packages=find_packages(include=['oriole_test']),
     test_suite='tests',
     url='https://github.com/zhouxiaoxiang/oriole-test',
-    version='0.4.0',
+    version='0.4.1',
     zip_safe=False,
 )
